7ecd1db0042c91a21f173f5c55f65d561974baa2
KanujanKing777 <kanujanpiragadeesh@gmail.com>
2024-11-14 09:57:04 +0530

    Brand new

---
+++ fun.js
@@ -0,0 +1,27 @@
+function clickingfun(i){
+    let currentUrl = window.location.href;
+
+    let path = window.location.pathname;
+
+    // Get the part before the last '/'
+    let preURL = path.substring(0, path.lastIndexOf('/'));
+
+    console.log(preURL);
+    let newurl = "";
+    // Example: Update an element's text with the modified value
+    if(i == 1){
+        newurl = "index.html";
+    }else if(i== 7){
+        newurl = "ContactUs.html";
+    }else if(i== 3){
+        newurl = "Locations.html";
+    }else if(i== 2){
+        newurl = "AboutUs.html";
+    }else if(i== 4){
+        newurl = "Tournaments.html";
+    }else if(i== 6){
+        newurl = "Articles.html";
+    }
+
+    window.open(preURL+'/'+newurl, "_self");
+}
